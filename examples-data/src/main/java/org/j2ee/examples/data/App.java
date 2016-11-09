359193dc138d459d4c749aa7e907a76fe7b47348
xiaogoubuli <xiaogoubuli@163.com>
2016-11-09 22:06:57 +0800

    add module examples-data

---
+++ examples-data/src/main/java/org/j2ee/examples/data/App.java
@@ -0,0 +1,13 @@
+package org.j2ee.examples.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
