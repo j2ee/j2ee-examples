fad660779373de52ba1829ec6572e17ed008cb8e
xiaogoubuli <xiaogoubuli@163.com>
2016-11-15 21:51:17 +0800

    add module hibernate

---
+++ examples-hibernate/src/main/java/org/j2ee/examples/hibernate/App.java
@@ -0,0 +1,13 @@
+package org.j2ee.examples.hibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
